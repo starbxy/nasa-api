aed94259f62e0c153c468c9b57cb33b9a4d3ee25
Kaan <osnapitzkaan@gmail.com>
2025-08-25 13:27:20 +0100

    trying to fetch events in 3d frame - failing?

--- Earth Observatory Natural Event Tracker [EONET].py
+++ Earth Observatory Natural Event Tracker [EONET].py
@@ -1,24 +1,32 @@
 import requests
 import os
+from dotenv import load_dotenv
 
 EONET_URL = "https://eonet.gsfc.nasa.gov/api/v3/events"
 
-num_events = 5
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
 
-def fetch_events(limit=num_events):
+def fetch_events():
     params = {
-        "limit": limit,  # number of events to fetch
-        "status": "open" # active events
+        "status": "open", # active events
+        "days": 3 # last 3 days - GMT/UTC
+        #"api_key": API_KEY
     }
     response = requests.get(EONET_URL, params=params)
     response.raise_for_status()
     return response.json()
 
-if __name__ == "__main__":
-    data = fetch_events(limit=num_events)
-    for event in data["events"]:
+def print_events(events):
+    for event in events["events"]:
         print(f"Title: {event['title']}")
         print(f"Category: {event['categories'][0]['title']}")
-        print(f"Date: {event['geometry'][0]['date']}")
+        print(f"Date (UTC): {event['geometry'][0]['date']}")
+        print(f"Description: {event['description']}")
         print(f"Coords: {event['geometry'][0]['coordinates']}")
-        print(f"Link: {event['link']}\n")
+        print(f"Link: {event['link']}")
+        print(f"Sources: {event['sources']}\n")
+
+if __name__ == "__main__":
+    data = fetch_events()
+    print_events(data)
